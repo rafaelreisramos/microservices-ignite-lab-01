44fc2a48fed174987cbd44eedff6b871edbeeec8
rafaelreisramos <rrramos@gmail.com>
2022-04-05 14:28:27 -0300

    feat: graphql create product mutation

--- purchases/src/http/graphql/resolvers/products.resolver.ts
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -1,13 +1,22 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Product } from '../models/product';
 import { ProductsService } from '../../../services/products.service';
+import { CreateProductInput } from '../inputs/create-product-input';
+import { UseGuards } from '@nestjs/common';
+import { AuthorizationGuard } from '../../../http/auth/authorization.guard';
 
 @Resolver()
 export class ProductsResolver {
-  constructor(private productService: ProductsService) {}
+  constructor(private productsService: ProductsService) {}
 
   @Query(() => [Product])
   products() {
-    return this.productService.listAllProducts();
+    return this.productsService.listAllProducts();
+  }
+
+  @UseGuards(AuthorizationGuard)
+  @Mutation(() => Product)
+  createProduct(@Args('data') data: CreateProductInput) {
+    return this.productsService.createProduct(data);
   }
 }

6e6a4241067b02df04d5d43367529f63ab570457
rafaelreisramos <rrramos@gmail.com>
2022-04-14 08:45:15 -0300

    chore: change queries and mutations
    - add support for private queries needed for Auth0

--- web/src/graphql/generated/next-page-public.tsx
+++ web/src/graphql/generated/next-page-public.tsx
@@ -6,44 +6,9 @@ import { NextRouter, useRouter } from 'next/router'
 import { QueryHookOptions, useQuery } from '@apollo/client';
 import * as Apollo from '@apollo/client';
 import type React from 'react';
-import { getApolloClient , ApolloClientContext} from '../../lib/withApollo';
-export async function getServerPageMe
-    (options: Omit<Apollo.QueryOptions<Types.MeQueryVariables>, 'query'>, ctx: ApolloClientContext ){
-        const apolloClient = getApolloClient(ctx);
-        
-        const data = await apolloClient.query<Types.MeQuery>({ ...options, query: Operations.MeDocument });
-        
-        const apolloState = apolloClient.cache.extract();
-
-        return {
-            props: {
-                apolloState: apolloState,
-                data: data?.data,
-                error: data?.error ?? data?.errors ?? null,
-            },
-        };
-      }
-export const useMe = (
-  optionsFunc?: (router: NextRouter)=> QueryHookOptions<Types.MeQuery, Types.MeQueryVariables>) => {
-  const router = useRouter();
-  const options = optionsFunc ? optionsFunc(router) : {};
-  return useQuery(Operations.MeDocument, options);
-};
-export type PageMeComp = React.FC<{data?: Types.MeQuery, error?: Apollo.ApolloError}>;
-export const withPageMe = (optionsFunc?: (router: NextRouter)=> QueryHookOptions<Types.MeQuery, Types.MeQueryVariables>) => (WrappedComponent:PageMeComp) : NextPage  => (props) => {
-                const router = useRouter()
-                const options = optionsFunc ? optionsFunc(router) : {};
-                const {data, error } = useQuery(Operations.MeDocument, options)    
-                return <WrappedComponent {...props} data={data} error={error} /> ;
-                   
-            }; 
-export const ssrMe = {
-      getServerPage: getServerPageMe,
-      withPage: withPageMe,
-      usePage: useMe,
-    }
+import { getApolloClient , ApolloClientContext} from '../../lib/withApolloPublic';
 export async function getServerPageGetProducts
-    (options: Omit<Apollo.QueryOptions<Types.GetProductsQueryVariables>, 'query'>, ctx: ApolloClientContext ){
+    (options: Omit<Apollo.QueryOptions<Types.GetProductsQueryVariables>, 'query'>, ctx?: ApolloClientContext ){
         const apolloClient = getApolloClient(ctx);
         
         const data = await apolloClient.query<Types.GetProductsQuery>({ ...options, query: Operations.GetProductsDocument });
---
+++ web/src/lib/withApolloPublic.tsx
@@ -0,0 +1,39 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  from,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from "@apollo/client"
+import { GetServerSidePropsContext, NextPage } from "next"
+
+export type ApolloClientContext = GetServerSidePropsContext
+
+export const withApolloPublic = (Component: NextPage) => {
+  return function Provider(props: any) {
+    return (
+      <ApolloProvider client={getApolloClient(undefined, props.apolloState)}>
+        <Component {...props} />
+      </ApolloProvider>
+    )
+  }
+}
+
+export function getApolloClient(
+  ctx?: ApolloClientContext,
+  ssrCache?: NormalizedCacheObject
+) {
+  const httpLink = createHttpLink({
+    uri: "http://localhost:3332/graphql",
+    fetch,
+  })
+
+  const cache = new InMemoryCache().restore(ssrCache ?? {})
+
+  return new ApolloClient({
+    connectToDevTools: true,
+    link: from([httpLink]),
+    cache,
+  })
+}
